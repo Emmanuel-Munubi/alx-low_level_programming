b6869c6849b6974f094b598052b354b6897495e5
Emmanuel-Munubi <manumunbi@gmail.com>
2021-07-12 17:34:56 +0300

    a program that prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,20 +7,9 @@
 *Description:
 *Return: 0
 */
-
 int main(int argc, char *argv[])
 {
-int count;
-if (argc > 1)
-{
-for (count = 1; count < argc; count++)
-{
-printf("%s\n", argv[count]);
-}
-}
-else
-{
-printf("the command had no other arguements.\n");
-}
-return (0);
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
 }
