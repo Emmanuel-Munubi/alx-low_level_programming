e47371d23dc185ff5b570df197f869d2a98d75ea
Emmanuel-Munubi <manumunbi@gmail.com>
2021-07-12 16:10:50 +0300

    update2 of 0-whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
 #include "holberton.h"
+#include <stdio.h>
 /**
 * main - main
-* @argc: Counts number of commandline arguments
-* @argv: array containing string commandline arguments as elements
+* @argc: command count/size of argv
+* @argv: array of commands
 *Description:
 *Return: 0
 */
-
 int main(int argc, char *argv[])
 {
 printf("%s\n", argv[argc - argc]);
