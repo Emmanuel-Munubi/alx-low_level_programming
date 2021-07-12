ba7390aab46e8b37c1b93d4a7f9870806ecb2a6d
Emmanuel-Munubi <manumunbi@gmail.com>
2021-07-12 16:55:26 +0300

    printing the count of commandline arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* main - main
+* @argc: counts the number of commandline arguments
+* @argv: array of pointers to strings
+*Description:
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+int count;
+argc = argc;
+for (count = 0; count == argc; count++)
+{
+printf("%d", count);
+}
+return (0);
