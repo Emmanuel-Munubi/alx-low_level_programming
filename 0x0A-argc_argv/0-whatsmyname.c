f9bfa9c5236de53a7429be846e747d2fc4510853
Emmanuel-Munubi <manumunbi@gmail.com>
2021-07-12 16:25:00 +0300

    update3 of whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 /**
 * main - main
-* @argc: command count/size of argv
-* @argv: array of commands
+* @argc: counts the number of commandline arguments
+* @argv: array of commands (strings as elemnts)
 *Description:
 *Return: 0
 */
 int main(int argc, char *argv[])
 {
-printf("%s\n", argv[argc - argc]);
+argc = argc;
+printf("%s\n", *argv);
 return (0);
 }
