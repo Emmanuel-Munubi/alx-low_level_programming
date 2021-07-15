c835fa965b0272da38af67990fc6888858e0e7c9
Emmanuel-Munubi <manumunbi@gmail.com>
2021-07-15 13:29:23 +0300

    added 0x0C-more_malloc_free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "holbrton.h"
+#include <stdlib.h>
+/**
+ * *malloc_checked - allocates memmory.
+ * @b: varia
+ * Return: pointer to the memmoryreserved.
+ */
+void *malloc_checked(unsigned int b)
+{
+  malloc(b)
+}
