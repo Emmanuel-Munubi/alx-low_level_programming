1fd7335349bf3d43d79b6ae4fad005e73d86d31c
Emmanuel-Munubi <manumunbi@gmail.com>
2021-07-15 21:06:18 +0300

    updated 1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,37 +4,36 @@
 
 /**
  * *string_nconcat - concatenates two strings.
- * @s1:
- * @s2:
- * @n:
+ * @s1: first string
+ * @s2: second string
+ * @n: size of memmory to be allocated for  s2
  * Return: pointer to a newly allocated space in memmory.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-int  count = 0;
-int i = 0;
-while (s2[i] != '\0')
-{
-count++;
-i++;
-if(n < count)
-{
-*s1 = (char *)malloc(n);
-}
-else
-{
-*s1 = (char *)malloc(count);
-}
-char *p = &s1;
-return (p);
-}
-
-int main(void)
-{
-    char *concat;
-
-    concat = string_nconcat("Holberton ", "School !!!", 6);
-    printf("%s\n", concat);
-    free(concat);
-    return (0);
+	int i;
+	int j;
+	if(s1 == NULL)
+		s1 = "";
+	if(s2 == NULL)
+		s2 = "";
+	i = 0;
+	while(s1[i] != '\0')
+		i++;
+	char* newstr = malloc(sizeof(char) * (i + n + 1));
+	if (newstr == NULL)
+		return (NULL);
+	i = j = 0;
+	while(s2 != '\0')
+	{
+		newstr[i] = s1[i]
+		i++;
+	}
+	while (j < n && s2[j] != '\0')
+	{
+		newstr[i] = s2[j];
+		i++, j++;
+	}
+	newstr[i] = '\0';
+	return (newstr);
 }
