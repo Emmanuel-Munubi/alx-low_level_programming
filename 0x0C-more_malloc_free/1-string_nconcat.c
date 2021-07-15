961da161a5f0ab14962ed1902852f6f344a473d7
Emmanuel-Munubi <manumunbi@gmail.com>
2021-07-15 22:23:33 +0300

    recorrected 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,26 +11,31 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int x1, x2, i, j;
-	char *s;
+	char *new;
+	int i;
+	unsigned int j;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (x1 = 0; s1[x1] != '\0'; x1++)
-	;
-	for (x2 = 0; s2[x2] != '\0'; x2++)
-	;
-	if (n >= x2)
-		n = x2;
-	s = malloc(sizeof(char) * (x1 + n + 1));
-	if (s == NULL)
+	i = 0;
+	while (s1[i] != '\0')
+		i++;
+	new = malloc(sizeof(char) * (i + n + 1));
+	if (new == NULL)
 		return (NULL);
-	for (i = 0; s1[i] != '\0'; i++)
-		s[i] = s1[i];
-	for (j = x1; j < x1 + n; j++)
-		s[j] = s2[j - x1];
-	s[j] = '\0';
-	return (s);
+	i = j = 0;
+	while (s1[i] != '\0')
+	{
+		new[i] = s1[i];
+		i++;
+	}
+	while (j < n && s2[j] != '\0')
+	{
+		new[i] = s2[j];
+		i++, j++;
+	}
+	new[i] = '\0';
+	return (new);
 }
