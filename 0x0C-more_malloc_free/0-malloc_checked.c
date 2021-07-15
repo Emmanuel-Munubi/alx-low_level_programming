cf4c3bab2a5f1e97814ce553b45578201b1a1837
Emmanuel-Munubi <manumunbi@gmail.com>
2021-07-15 13:57:58 +0300

    updated 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#include "holbrton.h"
+#include "holberton.h"
 #include <stdlib.h>
+#include <limits.h>
 /**
  * *malloc_checked - allocates memmory.
  * @b: varia
@@ -8,5 +9,26 @@
  */
 void *malloc_checked(unsigned int b)
 {
-  malloc(b)
+  int *p = (int *)malloc(b);
+}
+int main(void)
+{
+    char *c;
+    int *i;
+    float *f;
+    double *d;
+
+    c = malloc_checked(sizeof(char) * 1024);
+    printf("%p\n", (void *)c);
+    i = malloc_checked(sizeof(int) * 402);
+    printf("%p\n", (void *)i);
+    f = malloc_checked(sizeof(float) * 100000000);
+    printf("%p\n", (void *)f);
+    d = malloc_checked(INT_MAX);
+    printf("%p\n", (void *)d);
+    free(c);
+    free(i);
+    free(f);
+    free(d);
+    return (0);
 }
