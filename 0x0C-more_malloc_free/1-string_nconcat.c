ab86252b6f7542b1b56a15e8c7fa932ffa6a9c71
Emmanuel-Munubi <84630188+Emmanuel-Munubi@users.noreply.github.com>
2021-07-16 12:20:02 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	i = 0;
 	while (s1[i] != '\0')
 		i++;
-	new = malloc(sizeof(char) * (i + n + 1));
+	new = (char *)malloc(sizeof(char) * (i + n + 1));
 	if (new == NULL)
 		return (NULL);
 	i = j = 0;
