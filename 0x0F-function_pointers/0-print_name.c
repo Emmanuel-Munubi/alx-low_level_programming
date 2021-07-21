64de06639b74a2d07c9da67034052b6840f861b4
Emmanuel-Munubi <manumunbi@gmail.com>
2021-07-21 14:19:16 +0300

    corrected name print

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,24 +2,11 @@
 #include "function_pointers.h"
 
 /**
- * naming - naming - uses putchar to print the string
- */
-void naming(void)
-{
-	unsigned int i;
-
-	while (name[i])
-		if (name[i] >= 'a' && name[i] <= 'z')
-			putchar(name[i]);
-}
-/**
  * print_name - prints a name
- * @name: name of the person
- * @f: a pointer to function that accepts a string as an argument
+ * @name: the name
+ * @f: pointer to function
  */
 void print_name(char *name, void (*f)(char *))
 {
-	void (*f)(char *) = &naming;
-
 	f(name);
 }
