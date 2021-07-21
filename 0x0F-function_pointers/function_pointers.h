b476c3877b9594ae7f0119b9159cc376c5b80b0f
Emmanuel-Munubi <manumunbi@gmail.com>
2021-07-21 14:31:15 +0300

    updated function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,6 @@
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
+#include <stdlib.h>
 void print_name(char *name, void (*f)(char *));
 int putchar(const char *string);
-void naming(void);
+#endif
