fc97ec598521195d3dee0e2f76a04478ace03330
Emmanuel-Munubi <manumunbi@gmail.com>
2021-07-21 12:57:58 +0300

    added documentation

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,19 +2,22 @@
 #include "holberton.h"
 
 /**
+ * naming - naming - uses putchar to print the string
+ */
+void naming(void)
+{
+	unsigned int i;
+
+	while (name[i])
+		if (name[i] >= 'a' && name[i] <= 'z')
+			putchar(name[i]);
+}
+/**
  * print_name - prints a name
  * @name: name of the person
  * @f: a pointer to function that accepts a string as an argument
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	unsigned int i;
-	void naming()
-	{
-		while(name[i])
-			if(name[i] >= 'a' && name[i] <= 'z')
-				putchar(name[i]);
-	}
 	void (*f)(char *) = &naming;
 }
