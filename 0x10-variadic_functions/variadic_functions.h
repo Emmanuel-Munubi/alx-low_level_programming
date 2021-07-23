8248f8813f99d45bbb2ebea732f84cad68313991
Emmanuel-Munubi <manumunbi@gmail.com>
2021-07-23 06:09:19 +0300

    recorrected variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -10,5 +10,19 @@ void print_int(va_list list);
 void print_float(va_list list);
 void print_char(va_list list);
 void print_str(va_list list);
+int _putchar(char c);
+
+
+/**
+ * struct printTypeStruct - structure definition of a printTypeStruct
+ * @type: type
+ * @printer: function to print
+ */
+typedef struct printTypeStruct
+{
+char *type;
+void (*printer)(va_list);
+} printTypeStruct;
+
 
 #endif
