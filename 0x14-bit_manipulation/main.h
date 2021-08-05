a303325ca4e4e62bca7cddb20bee138e2752a77e
Emmanuel-Munubi <manumunbi@gmail.com>
2021-08-05 16:25:23 +0300

    task 0

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int binary_to_uint(const char *b);
+unsigned int _pow(unsigned int n, unsigned int p);
+
+#endif
